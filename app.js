ba167507cf6cfe8ca53929515654d60ad4946c68
Guoxin <suiguoxin@gmail.com>
2017-04-20 23:10:46 +0200

    add login and signup page

--- app.js
+++ app.js
@@ -9,6 +9,18 @@ app.use(express.static('public'));
 
 app.use('/', indexRouter);
 
+app.get('/login', function(req, res){
+	res.render('login', {
+		title:"OCEANY"
+	});
+});
+
+app.get('/signup', function(req, res){
+	res.render('signup', {
+		title:"OCEANY"
+	});
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
