c0797c7e678ed70920c64c013cb0fe8349577797
Guoxin <suiguoxin@gmail.com>
2017-04-23 22:26:08 +0200

    add the block to deal with the erreors

--- app.js
+++ app.js
@@ -25,6 +25,24 @@ app.get('/signup', function(req, res){
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
