e612e35f6e9456949725f4e0dae6fd7560d567d6
Guoxin <suiguoxin@gmail.com>
2017-05-02 21:37:20 +0200

    change port and script for deploying to heroku

--- app.js
+++ app.js
@@ -78,6 +78,7 @@ app.use(function (err, req, res, next) {
     res.render('error');
 });
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+const port = process.env.PORT || config.port;
+app.listen(port, function () {
+    console.log('${pkg.name} listening on port 3000!');
 });
