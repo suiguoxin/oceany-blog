eeb1a706ae37fa41883d5bf4543d4aca5b34612f
Guoxin <suiguoxin@gmail.com>
2017-05-10 22:55:14 +0200

    add softwares post page

--- routes/softwares.js
+++ routes/softwares.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var PostModel = require('../models/posts');
+var CommentModel = require('../models/comments');
 
 router.get('/', function (req, res) {
     var page = req.query.page ? req.query.page : 1;
@@ -20,6 +21,7 @@ router.get('/', function (req, res) {
             PostModel.getFivePostsBySection(page, section)
                 .then(function (result) {
                     res.render('softwares/index', {
+                        section:'softwares',
                         posts: result,
                         page: page,
                         isFirstPage: isFirstPage,
@@ -27,6 +29,24 @@ router.get('/', function (req, res) {
                         pagesCount: pagesCount
                     });
                 });
+        });
+});
+
+router.get('/:postId', function (req, res) {
+    var postId = req.params.postId;
+
+    Promise.all([
+        PostModel.getPostById(postId),
+        CommentModel.getComments(postId),
+        PostModel.incPv(postId)
+    ])
+        .then(function (result) {
+            var post = result[0];
+            var comments = result[1];
+            res.render('softwares/post', {
+                post: post,
+                comments: comments
+            });
         });
 });
 
