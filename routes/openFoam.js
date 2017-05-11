a18ce6032fc9b7cac31ebaefaa16a0a040be2d99
sgxgithub <suiguoxin@gmail.com>
2017-05-11 13:35:55 +0200

    change the style of generated menu in openFoam page

--- routes/openFoam.js
+++ routes/openFoam.js
@@ -10,7 +10,7 @@ router.get('/', function (req, res) {
         .then(function (result) {
             res.render('openFoam/index', {
                 posts: result,
-                section:"openFoam"
+                section: "openFoam"
             });
         });
 });
@@ -18,45 +18,24 @@ router.get('/', function (req, res) {
 router.get('/:postId', function (req, res) {
     var postId = req.params.postId;
 
-    Promise.all([
-        PostModel.getPostById(postId),
-        CommentModel.getComments(postId),
-        PostModel.incPv(postId)
-    ])
+    PostModel.getPostsBySection("OpenFoam")
         .then(function (result) {
-            var post = result[0];
-            var comments = result[1];
-            res.render('openFoam/post', {
-                post: post,
-                comments: comments
-            });
+            var posts = result;
+            Promise.all([
+                PostModel.getPostById(postId),
+                CommentModel.getComments(postId),
+                PostModel.incPv(postId)
+            ])
+                .then(function (result) {
+                    var post = result[0];
+                    var comments = result[1];
+                    res.render('openFoam/post', {
+                        posts: posts,
+                        post: post,
+                        comments: comments
+                    });
+                });
         });
 });
-
-// router.get('/:index', function (req, res) {
-//     var index = req.params.index;
-//
-//     PostModel.getPostBySectionAndIndex("OpenFoam", index)
-//         .then(function (result) {
-//             if (!result) {
-//                 req.flash('error', 'This post does not exist');
-//                 res.redirect('back');
-//                 return;
-//             }
-//             var post = result;
-//             var postId = post._id;
-//             Promise.all([
-//                 CommentModel.getComments(postId),
-//                 PostModel.incPv(postId)
-//             ])
-//                 .then(function (result) {
-//                     var comments = result[0];
-//                     res.render('openFoam/post', {
-//                         post: post,
-//                         comments: comments
-//                     });
-//                 });
-//         });
-// });
 
 module.exports = router;
