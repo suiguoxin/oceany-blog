05cb2164d95f9a9b3c477375418ff372d37ac63c
sgxgithub <suiguoxin@gmail.com>
2017-05-12 18:29:43 +0200

    get the img src in javascript

--- public/js/profile.js
+++ public/js/profile.js
@@ -2,10 +2,14 @@ function uploadAvatar() {
     var formData = new FormData();
     //why this line doesn't work
     //var avatar = $('#avatar').files[0];
-    avatar = document.getElementById("avatar").files[0];
+    var avatar = document.getElementById("avatar").files[0];
     formData.append("avatar", avatar);
+    formData.append("test","content of body test");
 
-    formData.append("test","content of test");
+    var src = document.getElementById("avatar").files[0].name;
+    alert(src);
+    //var avatarPreview = document.getElementById("avatarPreview");
+    $('#avatarPreview').attr("src",src);
 
     $.ajax({
         url: "/profile/uploadAvatar",
