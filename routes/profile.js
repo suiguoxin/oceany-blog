b874004c1db0e75224f7b5b37d1f55cedcd8717e
Guoxin <suiguoxin@gmail.com>
2017-05-16 22:39:29 +0200

    use qiniu to save avatar

--- routes/profile.js
+++ routes/profile.js
@@ -1,14 +1,19 @@
 var express = require('express');
 var router = express.Router();
-
+//multer
 var multer = require('multer');
-var storage = multer.diskStorage({
-    destination: 'public/uploads/avatars/',
-    filename: function (req, file, cb) {
-        cb(null, file.fieldname + '-' + Date.now());
-    }
+var memoryStorage = multer.memoryStorage();
+var uploadMemory = multer({storage: memoryStorage});
+//qiniu
+var qn = require('qn');
+var client = qn.create({
+    accessKey: 'IThkAny7wzlPGCR2pcJT8IrWkdes0Ic7PR38OwoQ',
+    secretKey: '4lUiAwT3jymK85eFu85PboWTjuv6wLM8wMO8oCEs',
+    bucket: 'oceany',
+    origin: 'http://oceany.u.qiniudn.com',
+    timeout: 3600000, // default rpc timeout: one hour
+    uploadURL: 'http://up-z2.qiniu.com/' // the app outside of China
 });
-var upload = multer({storage: storage});
 
 var UserModel = require('../models/users');
 
@@ -20,7 +25,8 @@ router.get('/', function (req, res) {
     });
 });
 
-router.post('/', upload.single('avatar'), function (req, res) {
+//do not deal with avatar here
+router.post('/', uploadMemory.single('avatar'), function (req, res) {
     var userId = req.session.user._id;
     var bio = req.body.bio;
 
@@ -33,21 +39,28 @@ router.post('/', upload.single('avatar'), function (req, res) {
         });
 });
 
-router.post('/uploadAvatar', upload.single('avatar'), function (req, res) {
+router.post('/uploadAvatar', uploadMemory.single('avatar'), function (req, res) {
     console.log(req.file);
     console.log(req.body);
 
     var userId = req.session.user._id;
     var avatar = req.file;
-    var src = "http://localhost:3000/uploads/avatars/" + avatar.filename;
 
-    UserModel.updateUserById(userId, {avatar: src})
-        .then(function () {
-            //update session
-            req.session.user.avatar = src;
-            //return img src to ajax
-            res.json({src: src});
-        });
+    //upload to qiniu
+    client.upload(avatar.buffer, {key: req.file.fieldname+ '-' + Date.now()}, function (err, result) {
+        console.log(result);
+        var url = 'http://oq29i4a0h.bkt.clouddn.com/';
+        var src = url+result.key;
+        //upload the user in db
+        UserModel.updateUserById(userId, {avatar: src})
+            .then(function () {
+                //update session
+                req.session.user.avatar = src;
+                //return img src to ajax
+                res.json({src: src});
+            });
+
+    });
 });
 
 module.exports = router;
