1d3173d58dcea64f5723a5a97f6c964072a881fd
sgxgithub <suiguoxin@gmail.com>
2017-06-01 16:18:54 +0200

    add collapse sidebar animation

--- public/js/posts.js
+++ public/js/posts.js
@@ -1,21 +1,32 @@
 (function ($) {
-    $(".updatePost").click(function (e) {
-        e.preventDefault();
+    // $(".updatePost").click(function (e) {
+    //     e.preventDefault();
+    //
+    //     let url = $(this).data("url");
+    //     console.log(url);
+    //
+    //     $.ajax({
+    //         method: "get",
+    //         url: url
+    //     }).done(function (result) {
+    //         //set post content and comments
+    //         $("#content").html(result);
+    //         //highlight code block
+    //         $('pre > code').each(function () {
+    //             hljs.highlightBlock(this);
+    //         });
+    //     });
+    // });
 
-        let url = $(this).data("url");
-        console.log(url);
-
-        $.ajax({
-            method: "get",
-            url: url
-        }).done(function (result) {
-            //set post content and comments
-            $("#content").html(result);
-            //highlight code block
-            $('pre > code').each(function () {
-                hljs.highlightBlock(this);
-            });
+    $('#sidebar')
+        .on('show.bs.collapse', function () {
+            $("#main").toggleClass("col-9 col-12");
+        })
+        .on('hidden.bs.collapse', function () {
+            setTimeout(
+                function () {
+                    $("#main").toggleClass("col-9 col-12");
+                }, 350);
         });
-    });
 
 })(jQuery);
