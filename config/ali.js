fd7fa0335f94882ba8eea0825cb051b0b95f6441
sgxgithub <suiguoxin@gmail.com>
2017-06-26 17:55:03 +0200

    add config for ali mongo

---
+++ config/ali.js
@@ -0,0 +1,20 @@
+module.exports = {
+    port: 3000,
+    session: {
+        secret: 'oceany',
+        key: 'oceany',
+        maxAge: 2592000000
+    },
+    mongodb: 'mongodb://localhost:27017/oceany-blog',
+
+    // qn
+    qn: {
+        accessKey: 'IThkAny7wzlPGCR2pcJT8IrWkdes0Ic7PR38OwoQ',
+        secretKey: '4lUiAwT3jymK85eFu85PboWTjuv6wLM8wMO8oCEs',
+        bucket: 'oceany-prod',
+        origin: 'http://oceany-prod.u.qiniudn.com',
+        cdn: 'http://orwbxzl93.bkt.clouddn.com/',
+        timeout: 3600000, // default rpc timeout: one hour
+        uploadURL: 'http://up-z0.qiniu.com/' // the app outside of China
+    }
+};
