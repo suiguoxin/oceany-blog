3928084a89069140418475c3332a8fc514efb987
Guoxin <suiguoxin@gmail.com>
2017-07-02 21:59:45 +0200

    add confirm delete modal, used to post delete and comment delete

--- assets/javascripts/oceany.js
+++ assets/javascripts/oceany.js
@@ -4,4 +4,9 @@
     }).ajaxStop(function () {
         $('#spinner').fadeOut();
     });
+
+    // confirm delete modal
+    $('#confirm-delete').on('show.bs.modal', function (e) {
+        $(this).find('.btn-ok').attr('href', $(e.relatedTarget).data('href'));
+    });
 }(jQuery));
