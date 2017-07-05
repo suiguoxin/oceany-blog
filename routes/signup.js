75446b1972a51cff951751d5a726b34effcc34ec
sgxgithub <suiguoxin@gmail.com>
2017-07-05 18:10:31 +0200

    add regex to control username; fix signup flash bug

--- routes/signup.js
+++ routes/signup.js
@@ -16,9 +16,12 @@ router.post('/', function (req, res) {
     let password = req.body.password;
     let repassword = req.body.repassword;
 
+    const patt = /^[a-z][\w\.]{2,22}[a-z0-9]$/i;
+
     try {
-        if (!(username.length >= 4 && username.length <= 10)) {
-            throw new Error('名字请限制在 4-10 个字符');
+        // if (!(username.length >= 4 && username.length <= 10)) {
+        if (!patt.test(username)) {
+            throw new Error("用户名以字母开头，长度在4到24之间，不能包含数字，字母，'_'和'.'以外的特殊字符");
         }
         if (password.length < 4) {
             throw new Error('密码至少 4 个字符');
@@ -28,12 +31,19 @@ router.post('/', function (req, res) {
         }
     } catch (e) {
         req.flash('error', e.message);
-        return res.render('signup', {
-            email: email,
-            username: username,
-            password: password,
-            repassword: repassword
-        });
+        /**
+         * with redirect we can't transfer params
+         * with render the flash message will not be refreshed
+         * TODO: transfer to AJAX
+         */
+        res.redirect('signup');
+    // , {
+    //         email: email,
+    //             username: username,
+    //             password: password,
+    //             repassword: repassword
+    //     }
+        return ;
     }
 
     // 待写入数据库的用户信息
