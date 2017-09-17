913d73597b0e2e8a50a41819680d21bdf2d29c2c
Guoxin <suiguoxin@gmail.com>
2017-09-17 15:34:39 +0800

    DEBUG: delete duplicated js; upload images to qiniu

---
+++ assets/javascripts/consult.js
@@ -0,0 +1,6 @@
+(function ($) {
+    $(document).ready(function(){
+        $("nav .navbar-nav .nav-item").removeClass("active");
+        $("#nav-item-consult").addClass("active");
+    });
+}(jQuery));
